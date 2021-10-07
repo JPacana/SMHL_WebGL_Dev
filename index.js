dee522bd48a73b4572f04629a018c16d3a1a4e91
JPacana <joshpacana@gmail.com>
2021-10-07 14:24:03 -0500

    This should totally work

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const path = require('path');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-const port = 8000;
- 
-app.use(express.static(path.join(__dirname, '/')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, + '/index.html'));
-})
- 
-http.createServer(app).listen(port, () => {
-    console.log('Server runnign on localhost:$(port)');
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-const { spawn } = require('child_process');
-const got = require('got');
-const test = require('tape');
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const port = 8000;
 
-// Start the app
-const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
+// Use the whole root as static files to be able to serve the html file and
+// the build folder
+app.use(express.static(path.join(__dirname, '/')));
 
-test('responds to requests', (t) => {
-  t.plan(4);
+// Send html on '/'path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, + '/index.html'));
+})
 
-  // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    // Make a request to our app
-    (async () => {
-      const response = await got('http://127.0.0.1:5000');
-      // stop the server
-      child.kill();
-      // No error
-      t.false(response.error);
-      // Successful response
-      t.equal(response.statusCode, 200);
-      // Assert content checks
-      t.notEqual(response.body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(response.body.indexOf("Getting Started on Heroku with Node.js"), -1);
-    })();
-  });
+// Create the server and listen on port
+http.createServer(app).listen(port, () => {
+    console.log(`Server running on localhost:${port}`);
 });
