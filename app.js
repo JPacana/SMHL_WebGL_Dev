9afade8beed099e551501b96048df8151c8065e3
JPacana <joshpacana@gmail.com>
2021-10-07 14:52:35 -0500

    I am so confident that this will work

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // create an express app
-const express = require("express")
-const app = express()
+const express = require("express");
+const path = require('path');
+const app = express();
 
 // use the express-static middleware
-app.use(express.static("/"))
+//app.use(express.static("/"))
+app.use(express.static(path.join(__dirname, '/')));
 
 // define the first route
 app.get("/", function (req, res) {
-	res.send("<h1>Hello World!</h1>")
+	res.sendFile(path.join(__dirname, + '/index.html'));
 })
 
 // start the server listening for requests
